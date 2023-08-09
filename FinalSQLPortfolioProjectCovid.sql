f0395bb4c18ab856a506c68c4ab11ffdff1191ff
Michael-Hauffe <141796115+Michael-Hauffe@users.noreply.github.com>
2023-08-08 22:21:26 -0500

    Update FinalSQLPortfolioProjectCovid.sql

--- FinalSQLPortfolioProjectCovid.sql
+++ FinalSQLPortfolioProjectCovid.sql
@@ -44,14 +44,12 @@ GROUP BY continent
 ORDER BY TotalDeathCount DESC
 
 --Using aggregate functions to calculate total percentage of global population killed by covid
-Create View TotalGlobalDeaths as
 SELECT SUM(new_cases) as Total_Cases,SUM(new_deaths) as Total_Deaths, (SUM(new_deaths)/SUM(new_cases))*100 as
 DeathPercentage
 FROM PortfolioProject..CovidDeaths
 WHERE continent is not null and new_cases <> 0
 
 --Using aggregate functions to calculate the percentage of population killed by covid for a particular day
-Create View DeathPercentage as 
 SELECT date,SUM(new_cases) as Total_Cases,SUM(new_deaths) as Total_Deaths, (SUM(new_deaths)/SUM(new_cases))*100 as
 DeathPercentage
 FROM PortfolioProject..CovidDeaths
@@ -59,7 +57,6 @@ WHERE continent is not null and new_cases <> 0
 GROUP BY Date
 
 --Using a partition to create a rolling count of total people vaccinated for each day
-Create View RollingPeopleVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 From PortfolioProject..CovidDeaths dea
@@ -92,7 +89,6 @@ From #PercentTotalDeaths
 
 
 --Use CTE to compare total vaccinations per hundred people with excess mortality per million people
-Create View PopvsVac as
 With PopvsVac (Continent,location,date,total_vaccinations_per_hundred, people_vaccinated_per_hundred,people_fully_vaccinated_per_hundred, excess_mortality_cumulative_per_million)
 as
 (
